d59d3dcb5f27fe2597d7a2d464baef9e91a256b3
ANG-OUSA <angousa168@gmail.com>
2024-05-16 18:56:05 +0700

    update

---
+++ src/main/java/gic/i4c/App.java
@@ -0,0 +1,13 @@
+package gic.i4c;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
